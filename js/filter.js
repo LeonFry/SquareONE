dd373a66fce61ce5507ba5d00bd7f480c74ee9e5
Leon <leon@leon-fry.com>
2015-05-18 09:10:23 -0500

    Added card sorting (needs improvement)

--- js/filter.js
+++ js/filter.js
@@ -40,4 +40,28 @@ calculateLIsInRow();
 $(window).resize(calculateLIsInRow);
 $(document).ready(calculateLIsInRow);
 
+$(document).ready(function() {
+  $('ul.sorter a').click(function() {
+    $(this).css('outline', 'none');
+    $('ul#filter .current').removeClass('current');
+    $(this).parent().addClass('current');
+
+    var filterVal = $(this).text().toLowerCase().replace(' ', '-');
+
+    if (filterVal == 'all') {
+      $('ul.card-sort li.hidden').fadeIn('slow').removeClass('hidden');
+    } else {
+      $('ul.card-sort li').each(function() {
+        if (!$(this).hasClass(filterVal)) {
+          $(this).fadeOut('normal').addClass('hidden');
+        } else {
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    }
+
+    return false;
+  });
+});
+
 
