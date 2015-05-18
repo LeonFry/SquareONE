b9875b926f35e9c7e9cf9b11db3020d8cae3a42e
Leon <leon@leon-fry.com>
2015-05-18 00:13:55 -0500

    Added dynamic sizing to filter bar (non-functional)

--- js/filter.js
+++ js/filter.js
@@ -2,5 +2,41 @@ $(function () {
 	$('.filter-trigger').on('click', function (e) {
 		$(this).toggleClass('active');
 		$(this).siblings('.filter-container').toggleClass('is-active');
+		$(this).siblings('.filter-selector').toggleClass('is-active');
 	})
 })
+$(document).ready(function(){
+    $(".load-button").click(function(){
+        alert("Width of div: " + $(".copy-cat").width());
+    });
+});
+/*window.onresize = function() {
+ $(document).ready(function(){
+      $(".copy-cat").width(masterwide);  
+});
+var masterwide = $(".dyno-width").width()+"px";
+}*/
+
+$(document).ready(function calculateLIsInRow() {
+    var lisInRow = 0;
+    $('ul li').each(function() {
+        if($(this).prev().length > 0) {
+            if($(this).position().top != $(this).prev().position().top) return false;
+            lisInRow++;
+        }
+        else {
+            lisInRow++;   
+        }
+    });
+var x = lisInRow;
+var y = 2;
+var masterwide = x * y + 'rem';
+$(".copy-cat").width(masterwide)
+})
+
+calculateLIsInRow();
+
+$(window).resize(calculateLIsInRow);
+
+
+
